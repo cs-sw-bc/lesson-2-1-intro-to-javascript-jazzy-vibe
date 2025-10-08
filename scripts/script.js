cab2b174c5eab461a4a81f93066f6163b54a4f0f
jazzy-vibe <jasmine.mt.ho@gmail.com>
2025-10-07 17:52:01 -0700

    updated lesson2.1

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+//external javascript
+let c=a+b;
+
+let name="me";
+
+let eligibility= false; //boolean
+
+let bug= null; //do not use reserved names - null, let, error
+
+let firstname= "First";
+let lastname= "Last";
+//camel casing
+
+
+//console log - display message for testing
+console.log("My full name is "+firstname+lastname);
+console.log(lastname);
+console.log("my last name is "+lastname);
+console.log(typeof(eligibility));
+
+//track variables to check if everything is good
+
+let x="2"; //string
+let y=1; //number
+let z=x+y; //21 - does not sum - puts variables together because one is a "string" and the other is a number
+
+//variable naming- 
+let name1; let _name; let name$;
+//let 1name; cannot start with a number
+
+//CONSTANTS
+const PI = 3.14529;
+const GRAVITY_OF_EARTH = 9.8; //SCREAMING SNAKE CASE
+
+
+let radius=2;
+
+let area = PI * (radius**2); //**FOR EXPONENTS
+
+//modulo returns the remainder of division operator
+let modulo = 37%10;
+modulo=c%a;
+
+let age=24;
+eligibility= age>18;
